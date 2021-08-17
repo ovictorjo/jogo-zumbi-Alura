435e9ccbf7647615050e7f4d79f7014b3ee448da
Joao Victor <ovictorjocorvinal@gmail.com>
2021-08-17 09:58:57 -0300

    Adicionando condição animação personagem

--- Assets/ControlaJogador.cs
+++ Assets/ControlaJogador.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class ControlaJogador : MonoBehaviour
 {
     public float velocidade = 15;
-    
+    private Animator _animator;
+
     // Update is called once per frame
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         float eixoX = Input.GetAxis("Horizontal");
@@ -16,5 +22,8 @@ public class ControlaJogador : MonoBehaviour
         Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
         
         transform.Translate(direcao * (velocidade * Time.deltaTime));
+
+
+        _animator.SetBool("Movendo", direcao != Vector3.zero);
     }
 }
